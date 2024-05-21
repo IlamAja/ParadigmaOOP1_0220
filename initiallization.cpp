924f58d085ab5bf8a1c94269d30c1792bb6e4227
Ilam Ball <informasiilam@gmail.com>
2024-05-21 14:50:18 +0700

    membuat output dari mahasiswa pada public

--- initiallization.cpp
+++ initiallization.cpp
@@ -9,6 +9,14 @@ class mahasiswa {
         float nilai;
 
     public:
-        mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) {}
+        mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) {
+            //member definisi
+        }
+        
+        ~mahasiswa(){
+            cout << "Id = " << id << endl;
+            cout << "Nama = " << nama << endl;
+            cout << "Nilai = " << nilai << endl;
+        }
         
 };
